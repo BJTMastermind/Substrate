cf738b0f1bb3d7f56d5fd18552331f5f9cbb65da
BJTMastermind <bjtmastermind@gmail.com>
2022-07-13 21:02:26 -0400

    Updated some files.

--- SubstrateCPPCLI/BlockReplace/BlockReplace/Program.cpp
+++ SubstrateCPPCLI/BlockReplace/BlockReplace/Program.cpp
@@ -10,9 +10,9 @@ using namespace Substrate::Core;
 
 // For a more advanced Block Replace example, see replace.cs in NBToolkit.
 
-int main (array<System::String^>^ args) 
+int main (array<System::String^>^ args)
 {
-	if (args->Length != 3) {
+    if (args->Length != 3) {
         System::Console::WriteLine("Usage: BlockReplace <world> <before-id> <after-id>");
         return 0;
     }
@@ -21,42 +21,42 @@ int main (array<System::String^>^ args)
     int before = System::Convert::ToInt32(args[1]);
     int after = System::Convert::ToInt32(args[2]);
 
-	// Open our world
-	NbtWorld^ world = NbtWorld::Open(dest);
+    // Open our world
+    NbtWorld^ world = NbtWorld::Open(dest);
 
-	// The chunk manager is more efficient than the block manager for
+    // The chunk manager is more efficient than the block manager for
     // this purpose, since we'll inspect every block
-	IChunkManager^ cm = world->GetChunkManager();
+    IChunkManager^ cm = world->GetChunkManager();
 
-	for each (ChunkRef^ chunk in cm) {
-		// You could hardcode your dimensions, but maybe some day they
+    for each (ChunkRef^ chunk in cm) {
+        // You could hardcode your dimensions, but maybe some day they
         // won't always be 16.  Also the CLR is a bit stupid and has
         // trouble optimizing repeated calls to Chunk.Blocks.xx, so we
-		// cache them in locals
-		int xdim = chunk->Blocks->XDim;
-		int ydim = chunk->Blocks->YDim;
-		int zdim = chunk->Blocks->ZDim;
+        // cache them in locals
+        int xdim = chunk->Blocks->XDim;
+        int ydim = chunk->Blocks->YDim;
+        int zdim = chunk->Blocks->ZDim;
 
-		// x, z, y is the most efficient order to scan blocks (not that
+        // x, z, y is the most efficient order to scan blocks (not that
         // you should care about internal detail)
-		for (int x = 0; x < xdim; x++) {
-			for (int z = 0; z < zdim; z++) {
-				for (int y = 0; y < ydim; y++) {
+        for (int x = 0; x < xdim; x++) {
+            for (int z = 0; z < zdim; z++) {
+                for (int y = 0; y < ydim; y++) {
 
-					// Replace the block with after if it matches before
-					if (chunk->Blocks->GetID(x, y, z) == before) {
-						chunk->Blocks->SetData(x, y, z, 0);
-						chunk->Blocks->SetID(x, y, z, after);
-					}
-				}
-			}
-		}
+                    // Replace the block with after if it matches before
+                    if (chunk->Blocks->GetID(x, y, z) == before) {
+                        chunk->Blocks->SetData(x, y, z, 0);
+                        chunk->Blocks->SetID(x, y, z, after);
+                    }
+                }
+            }
+        }
 
-		// Save the chunk
-		cm->Save();
+        // Save the chunk
+        cm->Save();
 
-		System::Console::WriteLine("Processed Chunk {0},{1}", chunk->X, chunk->Z);
-	}
+        System::Console::WriteLine("Processed Chunk {0},{1}", chunk->X, chunk->Z);
+    }
 
-	return 0;
+    return 0;
 }
